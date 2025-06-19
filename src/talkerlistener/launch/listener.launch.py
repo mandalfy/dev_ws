775a29fb4350ab43434129c055aa0bbee375d9ff
Shibam Mandal <shibammandal603@gmail.com>
2025-06-19 13:08:35 +0530

    Fixed syntax of launch files

--- src/talkerlistener/launch/listener.launch.py
+++ src/talkerlistener/launch/listener.launch.py
@@ -4,7 +4,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
     Node(
-        package='demo_nodes_py'
+        package='demo_nodes_py',
         executable='listener'
     )
     
